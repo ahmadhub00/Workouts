6c2c8e22af392dd1bae74dfbc7c0afe09552edea
Ahmad Taimur <ullahtaimur@gmail.com>
2025-01-13 10:00:00 +0500

    commit

--- chat-app/app/_layout.tsx
+++ chat-app/app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Slot } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { passkeys } from "@clerk/expo-passkeys";
+import { tokenCache } from "@/utils/cache";
 
 export default function RootLayout() {
-  return <Slot />;
+   const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error("Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file");
+  }
+  return (
+    <ClerkProvider
+    publishableKey={publishableKey}
+      tokenCache={tokenCache}
+      __experimental_passkeys={passkeys}>
+    <Slot />
+    </ClerkProvider>
+  );
 }
