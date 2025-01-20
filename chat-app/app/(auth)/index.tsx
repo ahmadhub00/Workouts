5de98b1c71180eedc7f7c385db71f0f5fa2114d4
Ahmad Taimur <ullahtaimur@gmail.com>
2025-01-20 10:00:00 +0500

    commit

--- chat-app/app/(auth)/index.tsx
+++ chat-app/app/(auth)/index.tsx
@@ -1,6 +1,6 @@
 import { View, Image, SafeAreaView } from "react-native";
 import {Text} from"@/components/Text";
-
+import {Button} from "@/components/Button";
 
 export default function Index() {
   return (
@@ -20,6 +20,24 @@ export default function Index() {
             Modern Chat App
           </Text>
         </View>
+        <Button> Text</Button>
+        <Button
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 10,
+            marginBottom: 30,
+          }}
+        >
+          <Image
+            source={require("@/assets/images/google-icon.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <Text style={{ color: "black", fontWeight: "500" }}>
+            Sign in with Google
+          </Text>
+        </Button>
     </SafeAreaView>
   );
 }
