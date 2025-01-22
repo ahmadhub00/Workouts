bf0d9a465aa8fe5281c7369aaeb9ff1a8f7a06ff
Ahmad Taimur <ullahtaimur@gmail.com>
2025-01-22 10:00:00 +0500

    commit

--- chat-app/app/(auth)/index.tsx
+++ chat-app/app/(auth)/index.tsx
@@ -1,17 +1,69 @@
+import * as React from "react";
 import { View, Image, SafeAreaView } from "react-native";
-import {Text} from"@/components/Text";
-import {Button} from "@/components/Button";
+
+import * as WebBrowser from "expo-web-browser";
+import * as AuthSession from "expo-auth-session";
+import {
+  isClerkAPIResponseError,
+  useSignIn,
+  useSSO,
+  useUser,
+} from "@clerk/clerk-expo";
+import { ClerkAPIError } from "@clerk/types";
+import { Text } from "@/components/Text";
+import { Button } from "@/components/Button";
+
+// Handle any pending authentication sessions
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Index() {
+  const { startSSOFlow } = useSSO();
+  const { user, isSignedIn } = useUser();
+  const { signIn, setActive } = useSignIn();
+  const [errors, setErrors] = React.useState<ClerkAPIError[]>([]);
+    const handleSignInWithPasskey =async () => {};
+  const handleSignInWithGoogle = React.useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } =
+        await startSSOFlow({
+          strategy: "oauth_google",
+          // Defaults to current path
+          redirectUrl: AuthSession.makeRedirectUri(),
+        });
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // Use the `signIn` or `signUp` returned from `startSSOFlow`
+        // to handle next steps
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      if (isClerkAPIResponseError(err)) setErrors(err.errors);
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, []);
+
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        
-      }}
-    >
-      <View style={{ flex:0.1}}/>
-      <View style={{ gap: 20, alignItems: "center" }}>
+      }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 16,
+        }}>
+        <View style={{ flex: 0.1 }} />
+        <View style={{ gap: 20, alignItems: "center" }}>
           <Image
             source={require("@/assets/images/logo.png")}
             style={{ width: 100, height: 100 }}
@@ -21,9 +73,11 @@ export default function Index() {
           </Text>
           <Text > The best chat app</Text>
         </View>
+        <View style={{ flex: 1 }} />
         <Button style={{
-            marginBottom: 20,
-          }}
+          marginBottom: 20,
+        }}
+        onPress={handleSignInwithPasskey}
         > Sign in with passkey</Button>
         <Button
           style={{
@@ -33,6 +87,7 @@ export default function Index() {
             gap: 10,
             marginBottom: 30,
           }}
+          onPress={handleSignInWithGoogle}
         >
           <Image
             source={require("@/assets/images/google-icon.png")}
@@ -42,6 +97,7 @@ export default function Index() {
             Sign in with Google
           </Text>
         </Button>
+        </View>
     </SafeAreaView>
   );
 }
