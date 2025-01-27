6d1b60ce664a3cc778599b08554b96e6bdcc4232
Ahmad Taimur <ullahtaimur@gmail.com>
2025-01-27 10:00:00 +0500

    commit

--- chat-app/app/(chat)/_layout.jsx
+++ chat-app/app/(chat)/_layout.jsx
@@ -1,20 +1,40 @@
 import { useUser } from "@clerk/clerk-expo";
 import { Redirect, Stack } from "expo-router";
+import {Image} from "react-native";
 
 export default function ChatLayout() {
    const { isSignedIn, isLoaded } = useUser();
-  
+  const {user } = useUser();
   // Wait for auth to load
   if (!isLoaded) {
     return null;
   }
-  
   // If signed in, redirect to auth section
   if (!isSignedIn) {
     return <Redirect href="/(auth)" />;
   }
-  // Show login screen for non-signed in users
-  return <Stack >
-    <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>;
+  
+  return ( 
+  <Stack >
+    <Stack.Screen name="index"
+     options={{ headerLargeTitle: true,
+      headerTitle: "Chat Rooms",
+      headerLeft: () => (
+            <Link href="/profile">
+              <Image
+                source={{ uri: user?.imageUrl }}
+                style={{ width: 32, height: 32, borderRadius: 16 }}
+              />
+            </Link>
+          ),
+          headerRight: () => (
+            <Link href="/new-room">
+              <IconSymbol name="plus" />
+            </Link>
+          ),
+        }}
+      />
+    <Stack.Screen name="profile" options={{ headerShown: false }} />
+   </Stack>
+  );
 }
